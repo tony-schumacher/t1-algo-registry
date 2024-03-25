1abeba0eae46aed34235213115208526849aaf43
tony-schumacher <ts@teclead-ventures.de>
2024-03-25 08:05:41 +0100

    automated building script

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # t1-algo-registry
 Registry for ML Model and Algotrader deployments.
-Build locally
-```sh
-./build.sh --tag transformer0.4.0
-```
 ![Architecture](docs/architecture.png)
--- build.sh
+++ build.sh
@@ -9,13 +9,10 @@ fi
 # Extract the tag value
 TAG=$2
 
-# Navigate to the models directory and build the Docker image in the background
-(cd ../t1-models && docker build -t teclead/t1-model:dev_${TAG} -f api/dockerfile.models .) &
+# Navigate to the models directory and build the Docker image
+cd ../t1-models && docker build -t teclead/t1-model:dev_${TAG} -f api/dockerfile.models .
 
-# Navigate to the m7 directory and build the Docker image in the background
-(cd ../m7 && docker build -t teclead/t1-m7:dev_${TAG} .) &
-
-# Wait for all background jobs to finish
-wait
+# Navigate to the m7 directory and build the Docker image
+cd ../m7 && docker build -t teclead/t1-m7:dev_${TAG} .
 
 echo "Docker images built successfully."
